6b05477ae50c60d65fe81ea44e10b2eb2275d80a
Lora11Lora22 <larisa345274578@gmail.com>
2022-09-14 13:47:20 +0300

    lctest

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,38 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class BaseTests {
+
+ private WebDriver driver;
+
+ public void setUp(){
+  System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+  driver = new ChromeDriver();
+  driver.get("https://the-internet.herokuapp.com/");
+
+
+   List<WebElement> links = driver.findElements(By.tagName("a"));
+   System.out.println(links.size());
+
+   WebElement inputsLink = driver.findElement(By.linkText("Angie"));
+   inputsLink.click();
+
+  System.out.println(driver.getTitle());
+  driver.quit();
+ }
+
+ public static void main(String args[]){
+  BaseTests test = new BaseTests();
+  test.setUp();
+ }
+
+
+
+}
