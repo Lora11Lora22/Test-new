f7c635ddf2a10578a4fc8980cc1b953c98499536
Lora11Lora22 <larisa345274578@gmail.com>
2022-09-14 19:56:09 +0300

    Chapter4.1

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,37 +5,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
 
- private WebDriver driver;
+    private WebDriver driver;
+    protected HomePage homePage;
 
- public void setUp(){
-  System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-  driver = new ChromeDriver();
-  driver.get("https://the-internet.herokuapp.com/");
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
+        driver.quit();
+    }
 
-  WebElement inputsLink = driver.findElement(By.linkText("Shifting Content"));
-  inputsLink.click();
-
-  WebElement inputsLink1 = driver.findElement(By.linkText("Example 1: Menu Element"));
-  inputsLink1.click();
-
-   List<WebElement> links = driver.findElements(By.tagName("li"));
-   System.out.println(links.size());
-
-
-  System.out.println(driver.getTitle());
-  driver.quit();
- }
-
- public static void main(String args[]){
-  BaseTests test = new BaseTests();
-  test.setUp();
- }
-
+    public static void main(String args[]) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
 
 
 }
