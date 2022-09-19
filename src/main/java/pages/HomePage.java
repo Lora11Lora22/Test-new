08df19af20fc9fcb4ae175fb34c9f1fa14940ffc
Lora11Lora22 <larisa345274578@gmail.com>
2022-09-19 10:43:12 +0300

    5 chapter

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -19,7 +19,10 @@ public class HomePage {
         clickLink("Dropdown");
         return new DropdownPage(driver);
     }
-
+public HoversPage clickHovers(){
+        clickLink("Hovers");
+        return new HoversPage(driver);
+    }
 
     private void clickLink(String linkText){
         driver.findElement(By.linkText(linkText)).click();
