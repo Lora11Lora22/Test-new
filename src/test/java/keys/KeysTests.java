49896505749d52a0af1afd1745c9c08af7b752fd
Lora11Lora22 <larisa345274578@gmail.com>
2022-09-19 21:27:16 +0300

    6 chapter

--- src/test/java/keys/KeysTests.java
+++ src/test/java/keys/KeysTests.java
@@ -13,7 +13,12 @@ public class KeysTests extends BaseTests {
         var keyPage = homePage.clickKeyPresses();
         keyPage.enterText("A" + Keys.BACK_SPACE);
         assertEquals(keyPage.getResult(),"You entered: BACK_SPACE");
+    }
 
+    @Test
+    public void testPi(){
+        var keyPage = homePage.clickKeyPresses();
+        keyPage.enterPi();
     }
 }
 
